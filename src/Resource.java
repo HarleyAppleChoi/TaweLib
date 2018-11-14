80fb270eb15af43e9757870ba272209d9ca24840
HarleyAppleChoi <appleno56@gmail.com>
2018-11-14 13:38:42 +0000

    Update src/Resource.java

---
+++ src/Resource.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Resource {
+	protected int ID;
+	protected String title;
+	protected int year;
+	protected Image thumbNailImage;
+	protected LinkedList BorrowItem;
+	protected LinkedList 
+}
