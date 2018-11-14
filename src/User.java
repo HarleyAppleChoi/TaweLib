e8e6a5c688ca17d0caeb0a49ce3b07530c5512c2
HarleyAppleChoi <appleno56@gmail.com>
2018-11-14 13:15:44 +0000

    Created setter and getter for User

---
+++ src/User.java
@@ -0,0 +1,47 @@
+
+public class User {
+	protected String username;
+	protected String firstName;
+	protected String lastName;
+	protected int mobileNo;
+	protected Image userImage;
+	
+	protected String getUsename() {
+		return username;
+	}
+	
+	protected String getFirstNamee() {
+		return firstName;
+	}
+
+	protected String getLastName() {
+		return lastName;
+	}
+	
+	protected int getMobileNo() {
+		return mobileNo;
+	}
+	
+	protected Image getUserImage() {
+		return userImage;
+	}
+	
+	protected void setUsername(String u) {
+		username = u;
+	}
+	
+	protected void setFirstName(String u) {
+		firstName = u;
+	}
+	protected void setLastName(String u) {
+		lastName = u;
+	}
+	protected void setMobileNo(int u) {
+		mobileNo = u;
+	}
+	protected void setImage(Image u) {
+		userImage = u;
+	}
+	
+	
+}
