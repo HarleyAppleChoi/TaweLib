3730371a44f20f6cf2fd0bd52c9b88f635a89805
HarleyAppleChoi <appleno56@gmail.com>
2018-11-28 12:54:29 +0000

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,34 @@
+CREATE TABLE resource (
+    id char(6) not null,
+    title char(70)not null,
+    _year char(30)not null,
+    image char(100)not null,
+    numAvCopies varchar(4)not null,
+    duration varchar(4)not null
+);
+
+CREATE TABLE book (
+    id char(6)not null,
+    author char(70) not null,
+    publisher char(70)not null,
+    genre char(100),
+    ISBN varchar(4),
+    _language varchar(4)
+    foreign key id references resource(id)
+);
+
+CREATE TABLE DVD (
+    id char(6) not null,
+    direction char(70) not null,
+    runtime char(70) not null,
+    _language char(100),
+    foreign key id references resource(id)
+);
+
+CREATE TABLE DVD_subtitle(
+    id char(10) not null,
+    subtitle char(30)
+    foreign key id reference DVD(id)
+);
+
+CREATE TABLE laptop
