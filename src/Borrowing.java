941b4e23702e8e8bf5f8956cda4472305f3dcee3
Emily Studley <emily.studley@gmail.com>
2018-11-28 13:31:16 +0000

    created borrowing class. added getters and setters

---
+++ src/Borrowing.java
@@ -0,0 +1,54 @@
+
+public class Borrowing {
+	
+	private int initialDate;
+	private int endDate;
+	private int returnDate;
+	private int borrowNo;
+	private normalUser user;
+	private Resource resourceType;
+	
+	
+	public int getInitialDate() {
+		return initialDate;
+	}
+	public void setInitialDate(int initialDate) {
+		this.initialDate = initialDate;
+	}
+	public int getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(int endDate) {
+		this.endDate = endDate;
+	}
+	public int getReturnDate() {
+		return returnDate;
+	}
+	public void setReturnDate(int returnDate) {
+		this.returnDate = returnDate;
+	}
+	public int getBorrowNo() {
+		return borrowNo;
+	}
+	public void setBorrowNo(int borrowNo) {
+		this.borrowNo = borrowNo;
+	}
+	public normalUser getUser() {
+		return user;
+	}
+	public void setUser(normalUser user) {
+		this.user = user;
+	}
+	public Resource getResourceType() {
+		return resourceType;
+	}
+	public void setResourceType(Resource resourceType) {
+		this.resourceType = resourceType;
+	}
+	
+	
+	
+	
+	
+	
+}
