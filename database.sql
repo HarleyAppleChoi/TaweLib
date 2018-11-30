dc6facadbda4a512692faba61434bdb089312b84
HarleyAppleChoi <appleno56@gmail.com>
2018-11-30 00:07:42 +0000

    Update database.sql
    all table are set(no data)

--- database.sql
+++ database.sql
@@ -38,7 +38,7 @@ add foreign key (resourceID) references resource(resourceID);
 
 CREATE TABLE DVD_subtitle (
     resourceID char(10) not null,
-    subtitle char(30) not null,
+    subtitle char(30) not null
     
 );
 
@@ -50,7 +50,7 @@ CREATE TABLE laptop (
     resourceID char(10) not null,
     manufacturer char(30)not null,
     model char(60) not null,
-    operatingSystem char(70) not null,
+    operatingSystem char(70) not null
     
 
 );
@@ -72,8 +72,7 @@ CREATE TABLE _user (
 
 CREATE TABLE normal_user (
     username char(40) not null,
-    balance int not null,
-
+    balance int not null
 );
 
 Alter table normal_user 
@@ -83,9 +82,7 @@ add foreign key (username) references _user(username);
 CREATE TABLE librarian (
     username char(40) not null,
     employmentDate char(30) not null,
-    staffNo int not null,
-    foreign key username references user(username)
-
+    staffNo int not null
 );
 
 Alter table librarian 
@@ -98,38 +95,52 @@ CREATE TABLE borrowing (
     dueDate char(10) not null,
     resourceID char(10) not null,
     onLoan char(30) not null,
-    primary key(borrowingID),
-    foreign key resourceID references resource(resourceID)
+    primary key(borrowingID)
 
 );
+
+Alter table borrowing 
+add foreign key (resourceID) references resource(resourceID);
+
 
 CREATE TABLE current_borrow_his (
     resourceID char(10) not null,
-    borrowingID not null,
-    foreign key resourceID references resource(resourceID),
-    foreign key borrowingID references borrowing(borrowingID)
-
+    borrowingID char(10) not null
 );
+
+Alter table current_borrow_his 
+add foreign key (resourceID) references resource(resourceID);
+
+Alter table current_borrow_his 
+add foreign key (borrowingID) references borrowing(borrowingID);
 
 CREATE TABLE current_borrowing (
     username char(30) not null,
-    borrowingID char(10) not null,
-    foreign key username references user(username),
-    foreign key borrowingID references borrowing(borrowingID)
-    
+    borrowingID char(10) not null    
 );
+
+Alter table current_borrowing
+add     foreign key (username) references user(username);
+
+
+Alter table current_borrowing
+add foreign key (borrowingID) references borrowing(borrowingID);
+
 
 CREATE TABLE returned_his (
     username char(30) not null,
-    borrowingID char(10) not null,
-    foreign key username references borrowing(borrowingID),
-    foreign key borrowingID references user(username)
-
+    borrowingID char(10) not null
 );
+
+Alter table returned_his
+add foreign key (username) references borrowing(borrowingID);
+
+Alter table returned_his
+add foreign key (borrowingID) references borrowing(borrowingID);
 
 CREATE TABLE transaction (
     transID char(10) not null,
-    debit/credit char(20) not null,
+    debit_credit char(20) not null,
     amount char(10) not null,
     primary key(transID)
 
@@ -137,32 +148,39 @@ CREATE TABLE transaction (
 
 CREATE TABLE transaction_his (
     username char(30) not null,
-    transID char(10) not null,
-    foreign key username references user(username),
-    foreign key transID references transaction(transID) 
-
+    transID char(10) not null
 );
+Alter table transaction_his
+add foreign key (username) references normal_user(username);
+    
+Alter table transaction_his
+add  foreign key (transID) references transaction(transID) ;
 
 CREATE TABLE overdue_transaction (
     transID char(10) not null,
-    borrowingID char(10) not null,
-    foreign key transID references transaction(transID),
-    foreign key borrowingID references borrowing(borrowingID)
-
+    borrowingID char(10) not null
 );
+Alter table overdue_transaction
+add foreign key (transID) references transaction(transID);
+Alter table overdue_transaction
+add   foreign key (borrowingID) references borrowing(borrowingID);
 
 CREATE TABLE reserved_item (
     username char(30) not null,
-    resourceID char(10) not null,
-    foreign key username references user(username),
-    foreign key resourceID references resource(resourceID)
-
+    resourceID char(10) not null
 );
+
+Alter table reserved_item
+add   foreign key (username) references normal_user(username);
+Alter table reserved_item
+add   foreign key (resourceID) references resource(resourceID);
 
 CREATE TABLE request_item (
     username char(30) not null,
-    resourceID char(10) not null,
-    foreign key username references user(username),
-    foreign key resourceID references resource(resourceID)
-
+    resourceID char(10) not null
+    
 );
+Alter table request_item
+add  foreign key (username) references normal_user(username);
+Alter table request_item
+add foreign key (resourceID) references resource(resourceID);
