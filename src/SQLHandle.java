7b4afe9252a55d24bb7ddb0b9cc1e137546b22b6
Emily Studley <emily.studley@gmail.com>
2018-12-01 14:44:22 +0000

    Update src/SQLHandle.java

--- src/SQLHandle.java
+++ src/SQLHandle.java
@@ -1,38 +1,63 @@
 
 import java.sql.*;
+
 //https://docs.oracle.com/javase/8/docs/api/index.html?java/sql/package-summary.html
 //https://www.roseindia.net/jdbc/introduction-to-the-java.sql.shtml
+/**
+ * This class connects the user to the database and has the set and get methods
+ * to retrieve information from it.
+ * 
+ * @author Hau Yi Choi
+ *
+ */
 public class SQLHandle {
-	
-	//more than 1 database is not allowed
-	
-	//your hostname
-	final static String HOST_NAME = "jdbc:mysql://localhost:3306/cw2";
-	//your SQL username
+
+	// more than 1 database is not allowed
+
+	// your hostname
+	final static String HOST_NAME = "jdbc:mysql://localhost:3306/cw230";
+	// your SQL username
 	final static String USERNAME = "root";
-	//your password
+	// your password
 	final static String PASSWORD = "123";
 	final static String GETALLRESOURCES = " ";
 	static Statement statement = null;
 	static Connection conn = null;
 
-	public SQLHandle(){
-		
-	try {
-		conn = DriverManager.getConnection(HOST_NAME,USERNAME,PASSWORD);
-		statement= conn.createStatement();
-		
-	} catch (SQLException e) {
-		e.printStackTrace();
-	} 
-	System.out.println("Success!");
-}
-	
+	/**
+	 * This method connects the user to the database.
+	 */
+	public SQLHandle() {
+
+		try {
+			conn = DriverManager.getConnection(HOST_NAME, USERNAME, PASSWORD);
+			statement = conn.createStatement();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Success!");
+	}
+
+	/**
+	 * This method allows us to create an SQL query in java that will link back to
+	 * the database.
+	 * 
+	 * @param query
+	 * @return the result of the query
+	 * @throws SQLException
+	 */
 	public static ResultSet get(String query) throws SQLException {
 		ResultSet result = statement.executeQuery(query);
 		return result;
 	}
-	
+
+	/**
+	 * This method allows us to edit the database from a query.
+	 * 
+	 * @param query
+	 * @throws SQLException
+	 */
 	public static void set(String query) throws SQLException {
 		statement.executeUpdate(query);
 	}
