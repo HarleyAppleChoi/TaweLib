59ea3e2e234d602ea9de52e2940a7695fc4c8419
Emily Studley <emily.studley@gmail.com>
2018-12-01 12:28:14 +0000

    added javadoc comments to DVD

--- src/Book.java
+++ src/Book.java
@@ -2,24 +2,24 @@ import com.sun.prism.Image;
 
 /**
  * This class creates an instance of a Book.
- * @author Emily Studley
- * @ version 2.1
+ * 
+ * @author Emily Studley @ version 2.1
  * 
  */
 class Book extends Resource {
 
-private String author;
-private String publisher;
-private String genre;
-private int isbn;
-private String language;
-private int fineAmount = 2;
-private int maxFineAmount = 25;
+	private String author;
+	private String publisher;
+	private String genre;
+	private int isbn;
+	private String language;
+	private int fineAmount = 2;
+	private int maxFineAmount = 25;
 
-
-
-    /**
-     * Constructor to construct a Book using the parameters from this class and the parameter from the superclass Resource.
+	/**
+	 * Constructor to construct a Book using the parameters from this class and the
+	 * parameter from the superclass Resource.
+	 * 
 	 * @param id
 	 * @param title
 	 * @param year
@@ -32,128 +32,143 @@ private int maxFineAmount = 25;
 	 * @param isbn
 	 * @param langauge
 	 */
-	public Book (int id, String title, int year, Image thumbNailImage, int numCopies, int numAvailableCopies, String author, String publisher, String genre, int isbn, String langauge) {
-		
+	public Book(int id, String title, int year, Image thumbNailImage, int numCopies, int numAvailableCopies,
+			String author, String publisher, String genre, int isbn, String langauge) {
+
 		super.id = id;
 		super.title = title;
 		super.year = year;
 		super.thumbNailImage = thumbNailImage;
 		super.numCopies = numCopies;
-		super.numAvailableCopies =  numAvailableCopies;
+		super.numAvailableCopies = numAvailableCopies;
 		this.author = author;
 		this.publisher = publisher;
 		this.genre = genre;
 		this.isbn = isbn;
 		this.language = langauge;
 	}
-	
-    /**
-     * Get method to get the author.
+
+	/**
+	 * Get method to get the author.
+	 * 
 	 * @return author
 	 */
-	public String getAuthor(){
+	public String getAuthor() {
 		return author;
 	}
-	
+
 	/**
 	 * Set method to set the author.
+	 * 
 	 * @param author
 	 */
-	public void setAuthor(String author){
+	public void setAuthor(String author) {
 		this.author = author;
 	}
-	
-    /**
-     * Get method to get the publisher.
+
+	/**
+	 * Get method to get the publisher.
+	 * 
 	 * @return publisher
 	 */
-	public String getPublisher(){
+	public String getPublisher() {
 		return publisher;
 	}
-    
-    /**
-     * Set method to set the publisher.
+
+	/**
+	 * Set method to set the publisher.
+	 * 
 	 * @param publisher
 	 */
-	public void setPublisher(String publisher){
+	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
-	
+
 	/**
 	 * Get method to get the genre.
+	 * 
 	 * @return genre
 	 */
-	public String getGenre(){
+	public String getGenre() {
 		return genre;
 	}
-    
-    /**
-     * Set method to set the genre.
+
+	/**
+	 * Set method to set the genre.
+	 * 
 	 * @param genre
 	 */
-	public void setGenre(String genre){
-		this.genre= genre;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
-    
-    /**
-     * Get method to get the isbn.
+
+	/**
+	 * Get method to get the isbn.
+	 * 
 	 * @return isbn
 	 */
-	public int getISBN(){
+	public int getISBN() {
 		return isbn;
 	}
-   
-    /**
-     * Set method to set the isbn.
+
+	/**
+	 * Set method to set the isbn.
+	 * 
 	 * @param isbn
 	 */
-	public void setISBN(int isbn){
+	public void setISBN(int isbn) {
 		this.isbn = isbn;
 	}
-    
-    /**
-     * Get method to get the language.
+
+	/**
+	 * Get method to get the language.
+	 * 
 	 * @return language
 	 */
-	public String getLanguage(){
+	public String getLanguage() {
 		return language;
 	}
-	
-    /**
-     * Set method to set the language.
+
+	/**
+	 * Set method to set the language.
+	 * 
 	 * @param language
 	 */
-	public void setLanguage(String language){
+	public void setLanguage(String language) {
 		this.language = language;
 
 	}
-	
+
 	/**
 	 * Get method to get the maximum fine amount.
+	 * 
 	 * @return maxFineAmount
 	 */
 	public int getMaxFineAmount() {
 		return maxFineAmount;
 	}
-	
+
 	/**
 	 * Set method to set the maximum fine amount.
+	 * 
 	 * @param maxFineAmount
 	 */
 	public void setMaxFineAmount(int maxFineAmount) {
 		this.maxFineAmount = maxFineAmount;
 	}
-	
-   /**
-    * Get method to get the fine amount per day.
-	* @return
-    */
-		public int getFineAmount() {
+
+	/**
+	 * Get method to get the fine amount per day.
+	 * 
+	 * @return
+	 */
+	public int getFineAmount() {
 		return fineAmount;
 	}
-	
+
 	/**
 	 * Set method to set the fine amount.
+	 * 
 	 * @param fineAmount
 	 */
 	public void setFineAmount(int fineAmount) {
--- src/DVD.java
+++ src/DVD.java
@@ -2,6 +2,13 @@
 import java.util.ArrayList;
 import com.sun.prism.Image;
 
+/**
+ * This class creates an instance of a DVD.
+ * 
+ * @author Emily Studley
+ * @version 2.1
+ *
+ */
 public class DVD extends Resource {
 	private String director;
 	private int runtime;
@@ -9,55 +16,113 @@ public class DVD extends Resource {
 	ArrayList<String> subLanguages = new ArrayList<String>();
 	private int fineAmount = 2;
 	private int maxFineAmount = 25;
-	
-public DVD(int id, String title, int year, Image thumbNailImage, int numCopies, int numAvailableCopies, String director, int runtime, String language, ArrayList<String> subLanguages) {
-		
+
+	/**
+	 * Constructor to construct a DVD using the parameters from this class and the
+	 * parameters from the superclass Resource.
+	 * 
+	 * @param id
+	 * @param title
+	 * @param year
+	 * @param thumbNailImage
+	 * @param numCopies
+	 * @param numAvailableCopies
+	 * @param director
+	 * @param runtime
+	 * @param language
+	 * @param subLanguages
+	 */
+	public DVD(int id, String title, int year, Image thumbNailImage, int numCopies, int numAvailableCopies,
+			String director, int runtime, String language, ArrayList<String> subLanguages) {
+
 		super.id = id;
 		super.title = title;
 		super.year = year;
 		super.thumbNailImage = thumbNailImage;
 		super.numCopies = numCopies;
-		super.numAvailableCopies =  numAvailableCopies;
+		super.numAvailableCopies = numAvailableCopies;
 		this.director = director;
 		this.runtime = runtime;
 		this.language = language;
 		this.subLanguages = subLanguages;
 	}
-	
+
+	/**
+	 * Get method to get the director.
+	 * @return
+	 */
 	public String getDirector() {
 		return director;
 	}
+
+	/**
+	 * Set method to set the director.
+	 * @param director
+	 */
 	public void setDirector(String director) {
 		this.director = director;
 	}
+
+	/**
+	 * Get method to get the runtime.
+	 * @return runtime
+	 */
 	public int getRuntime() {
 		return runtime;
 	}
+
+	/**
+	 * Set method to set the runtime.
+	 * @param runtime
+	 */
 	public void setRuntime(int runtime) {
 		this.runtime = runtime;
 	}
+
+	/**
+	 * Get method to get the language.
+	 * @return language
+	 */
 	public String getLanguage() {
 		return language;
 	}
+
+	/**
+	 * Set method to set the language.
+	 * @param language
+	 */
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-	
+
+	/**
+	 * Get method to get the maximum fine amount.
+	 * @return maxFineAmount
+	 */
 	public int getMaxFineAmount() {
 		return maxFineAmount;
 	}
-	
-	
+
+	/**
+	 * Set method to set the maximum fine amount.
+	 * @param maxFineAmount
+	 */
 	public void setMaxFineAmount(int maxFineAmount) {
 		this.maxFineAmount = maxFineAmount;
 	}
-	
-	
-		public int getFineAmount() {
+
+	/**
+	 * Get method to get the fine amount per day.
+	 * @return fineAmount
+	 */
+	public int getFineAmount() {
 		return fineAmount;
 	}
-	
-	
+
+	/**
+	 * Set method to set the fine amount.
+	 * @param fineAmount
+	 */
 	public void setFineAmount(int fineAmount) {
 		this.fineAmount = fineAmount;
 	}
