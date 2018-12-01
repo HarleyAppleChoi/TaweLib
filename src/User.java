c7362b1834d45c7665bad8282be8871c52241900
jwana004 <901526@swansea.ac.uk>
2018-12-01 13:54:31 +0000

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,5 +1,11 @@
 import javafx.scene.image.Image;
 
+/**
+ * user.java
+ * @author Jwana Abdalah
+ *
+ */
+
 public class User {
 	protected String username;
 	protected String firstName;
@@ -7,39 +13,73 @@ public class User {
 	protected int mobileNo;
 	protected Image userImage;
 	
+	/**
+	 * @return username
+	 */
 	protected String getUsename() {
 		return username;
 	}
 	
+	/**
+	 * @return firstName
+	 */
 	protected String getFirstNamee() {
 		return firstName;
 	}
-
+    
+    /**
+	 * @return lastName
+	 */
 	protected String getLastName() {
 		return lastName;
 	}
-	
+	/**
+	 * @return mibile number
+	 */
 	protected int getMobileNo() {
 		return mobileNo;
 	}
 	
+	/**
+	 * @return user Image
+	 */
 	protected Image getUserImage() {
 		return userImage;
 	}
 	
+	/**
+	 * method to set the username
+	 * @param u
+	 */
 	protected void setUsername(String u) {
 		username = u;
 	}
 	
+	/**
+	 * method to set firstName
+	 * @param u
+	 */
 	protected void setFirstName(String u) {
 		firstName = u;
 	}
+	/**
+	 * method to set lastName
+	 * @param u
+	 */
 	protected void setLastName(String u) {
 		lastName = u;
 	}
+	/**
+	 * method to set mobileNo
+	 * @param u
+	 */
 	protected void setMobileNo(int u) {
 		mobileNo = u;
 	}
+	/**
+	 * method to set the image
+	 * @param u
+	 */
 	protected void setImage(Image u) {
 		userImage = u;
 	}
