751881742a828d8b97ebbcd507560449823e1559
Iestyn Price <iestynprice196@gmail.com>
2018-12-06 18:53:46 +0000

    edit dvd controller

---
+++ src/EditDvdGuiController.java
@@ -0,0 +1,63 @@
+/**
+@author Iestyn Price
+*/
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class EditDvdGuiController {
+
+	@FXML
+	TextField enterID;
+	
+	@FXML
+	TextField enterTitle;
+	
+	@FXML
+	TextField enterYear;
+	
+	@FXML
+	TextField enterDirector;
+	
+	@FXML
+	TextField enterRunTime;
+	
+	@FXML
+	TextField enterLanguage;
+	
+	@FXML
+	Button cancelButton;
+	
+	@FXML
+	Button editDvdDetails;
+	
+	@FXML
+	CheckBox selectEnglish;
+	
+	@FXML
+	CheckBox selectSpanish;
+	
+	@FXML
+	CheckBox selectGerman;
+	
+	@FXML
+	CheckBox selectFrench;
+	
+	@FXML
+	CheckBox selectJapanese;
+	
+	@FXML
+	CheckBox selectOther;
+	
+	@FXML
+	ImageView dvdImage;
+
+
+
+
+
+
+}
