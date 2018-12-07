f93d4aa06d529afc37d9fe4a8986e659e2a4f82e
HarleyAppleChoi <appleno56@gmail.com>
2018-12-07 18:44:58 +0000

    updated database

--- src/Test.java
+++ src/Test.java
@@ -25,6 +25,7 @@ public class Test {
 			Librarian b = new Librarian();
 			b.addBook("aaa", "123", "adfsa", 1, 8, "asdf", "asdfa", "asdf", "12341", "asdf");
 			b.addDvd("wra", "12ss3", "aaaasdfasf", 2, 0, "sssf", "ssasdfa","sssasdf" ,"sssssssadas,sfddf");
+			b.addLaptop("asdfa", "1002", "jpeg", 42, 34, "apple", "macbook", "high sherra");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
