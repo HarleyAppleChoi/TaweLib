062228c1f4efbacfd3b3fd624cfa7e44d521ab3b
James Hogg <44004637+h099@users.noreply.github.com>
2018-12-09 15:23:06 +0000

    label

--- src/ControllerLibGui.java
+++ src/ControllerLibGui.java
@@ -14,6 +14,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -242,6 +243,12 @@ public class ControllerLibGui {
 	
 	@FXML
 	TextArea textReturn;
+	
+	@FXML
+	Label labelBorrow;
+	
+	@FXML
+	Label labelReturn;
 	
 	@FXML
 	private void searchAdd(ActionEvent e) {
