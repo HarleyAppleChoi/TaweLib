225e2fdc6a427c3c60a1ad7c8e66f2d97062345b
HarleyAppleChoi <appleno56@gmail.com>
2018-12-09 19:02:29 +0000

    Get path for thumbnail Image

--- src/CreateDVDController.java
+++ src/CreateDVDController.java
@@ -6,11 +6,13 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -72,7 +74,9 @@ public class CreateDVDController {
 	private void handleCreateDVDEvent(ActionEvent e) throws NumberFormatException, SQLException {
 		SQLHandle c = new SQLHandle();
 		Librarian l = new Librarian();
-		l.addDvd(dvdTitle.getText(), dvdYear.getText(), "", Integer.parseInt(numberOfCopiesDVD.getText()), Integer.parseInt(durationDVD.getText()),dvdDirector.getText(),dvdLanguage.getText(), dvdRuntime.getText(), dvdSubtitle.getText());
+		l.addDvd(dvdTitle.getText(), dvdYear.getText(), path, Integer.parseInt(numberOfCopiesDVD.getText())
+				, Integer.parseInt(durationDVD.getText()),dvdDirector.getText(),dvdLanguage.getText()
+				, dvdRuntime.getText(), dvdSubtitle.getText());
 		
 		
 		try {
@@ -99,32 +103,20 @@ public class CreateDVDController {
 		}
 	}
 
+	
+	private String path;
 	@FXML
-	private void handleCreateDVDImage(ActionEvent e) {
+	private void handleCreateDVDImage(ActionEvent e) throws IOException, SQLException {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
 		File file = fileChooser.showOpenDialog(new Stage());
 		if(file != null) {
 	        String imagepath = file.getPath();
 	        System.out.println("file:"+imagepath);
-	        Image image = new Image(imagepath);
+	        //Image image = new Image(imagepath);
 	        
-	        SQLHandle sql = new SQLHandle();
-			ResultSet r = sql.nonStaticGet("Select max(image) from user_;");
-			int number = 0;
-			while(r.next()) {
-				number = r.getInt("max(image)")+1;
-			}
-	        
-			BufferedImage bimage = new BufferedImage((int)image.getWidth(), (int)image.getHeight(),BufferedImage.TYPE_INT_RGB);
-
-		    // Draw the image on to the buffered image
-		    Graphics2D bGr = bimage.createGraphics();
-		    bGr.drawImage(image, null, null);
-		    bGr.dispose();
-	        ImageIO.write(image, "png", "");
-	        
-	        //imageView.setImage(image);
+	        path = imagepath;
+			
 	    }
 	    else
 	    {
@@ -135,15 +127,6 @@ public class CreateDVDController {
 	        alert.showAndWait();
 	    }
 	
-		try {
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DrawGUI.fxml"));
-			 Parent root = (Parent)fxmlLoader.load();
-			 Scene scene= new Scene(root); 
-			 Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
-			 window.setScene(scene);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
 	}
 	}
 
