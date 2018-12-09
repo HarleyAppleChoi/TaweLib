898b96cb06f0e9523c77209784a71be19297e57c
HarleyAppleChoi <appleno56@gmail.com>
2018-12-09 18:34:50 +0000

    Update CreateDVDController.java

--- src/CreateDVDController.java
+++ src/CreateDVDController.java
@@ -3,7 +3,13 @@
 */
 
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,10 +17,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class CreateDVDController {
 	
@@ -91,6 +101,40 @@ public class CreateDVDController {
 
 	@FXML
 	private void handleCreateDVDImage(ActionEvent e) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		File file = fileChooser.showOpenDialog(new Stage());
+		if(file != null) {
+	        String imagepath = file.getPath();
+	        System.out.println("file:"+imagepath);
+	        Image image = new Image(imagepath);
+	        
+	        SQLHandle sql = new SQLHandle();
+			ResultSet r = sql.nonStaticGet("Select max(image) from user_;");
+			int number = 0;
+			while(r.next()) {
+				number = r.getInt("max(image)")+1;
+			}
+	        
+			BufferedImage bimage = new BufferedImage((int)image.getWidth(), (int)image.getHeight(),BufferedImage.TYPE_INT_RGB);
+
+		    // Draw the image on to the buffered image
+		    Graphics2D bGr = bimage.createGraphics();
+		    bGr.drawImage(image, null, null);
+		    bGr.dispose();
+	        ImageIO.write(image, "png", "");
+	        
+	        //imageView.setImage(image);
+	    }
+	    else
+	    {
+	        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	        alert.setTitle("Information Dialog");
+	        alert.setHeaderText("Please Select a File");
+	        /*alert.setContentText("You didn't select a file!");*/
+	        alert.showAndWait();
+	    }
+	
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DrawGUI.fxml"));
 			 Parent root = (Parent)fxmlLoader.load();
@@ -101,5 +145,5 @@ public class CreateDVDController {
 			ex.printStackTrace();
 		}
 	}
+	}
 
-}
